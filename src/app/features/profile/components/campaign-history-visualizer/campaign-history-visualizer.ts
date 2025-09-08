341e3b8d4af4ca90653a223263838b314e96d03c
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-09-08 03:45:16 +0000

    Adiciona funcionalidade para visualizar campanha de cada character na pagina de perfil

--- src/app/features/profile/components/campaign-history-visualizer/campaign-history-visualizer.ts
+++ src/app/features/profile/components/campaign-history-visualizer/campaign-history-visualizer.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProfileService } from '../../services/profile-service';
 
 @Component({
   selector: 'app-campaign-history-visualizer',
   imports: [],
   templateUrl: './campaign-history-visualizer.html',
-  styleUrl: './campaign-history-visualizer.css'
+  styleUrl: './campaign-history-visualizer.css',
 })
 export class CampaignHistoryVisualizer {
-
+  profileService = inject(ProfileService);
 }
--- src/app/features/profile/profile.ts
+++ src/app/features/profile/profile.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { SplitterModule } from 'primeng/splitter';
 import { CharacterVisualizer } from './components/character-visualizer/character-visualizer';
 import { CampaignVisualizer } from './components/campaign-visualizer/campaign-visualizer';
+import { CampaignHistoryVisualizer } from './components/campaign-history-visualizer/campaign-history-visualizer';
 
 @Component({
   selector: 'app-profile',
-  imports: [SplitterModule, CharacterVisualizer, CampaignVisualizer],
+  imports: [SplitterModule, CharacterVisualizer, CampaignVisualizer, CampaignHistoryVisualizer],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
