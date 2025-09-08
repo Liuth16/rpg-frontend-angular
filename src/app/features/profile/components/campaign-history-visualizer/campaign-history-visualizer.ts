b7e04f307d089e683c5973e0de2ba032147c88de
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-09-08 18:20:18 +0000

    Adiciona dataview e cards do primeng no campaign history visualizer do perfil

--- src/app/features/profile/components/campaign-history-visualizer/campaign-history-visualizer.ts
+++ src/app/features/profile/components/campaign-history-visualizer/campaign-history-visualizer.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProfileService } from '../../services/profile-service';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DataViewModule } from 'primeng/dataview';
 
 @Component({
   selector: 'app-campaign-history-visualizer',
-  imports: [],
+  imports: [ButtonModule, CardModule, DataViewModule],
   templateUrl: './campaign-history-visualizer.html',
   styleUrl: './campaign-history-visualizer.css',
 })
